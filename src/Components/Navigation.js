c5fc1853b7058a43f415213a0d907ee3c745a9d9
Disha <disha.baushali@terralogic.com>
2020-12-28 09:20:24 +0530

    bootstrap4

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import { Button,Navbar,Nav,NavDropdown,Form,FormControl } from 'react-bootstrap'
+import { Navbar,Nav,NavDropdown } from 'react-bootstrap'
 import "./style.scss"
 import { DiAndroid } from "react-icons/di";
 export class Navigation extends Component {
     render() {
         return (
-            <div>
+            <div className="demo">
                 <Navbar bg="primary"  variant="dark">
                 <DiAndroid/>
   <Navbar.Brand href="">Welcome</Navbar.Brand>
